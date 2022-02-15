d9fccf23152bffd4c1d3613cbc20e0de74ed7195
mdussauz <mdussauz@cshl.edu>
2022-02-14 21:00:17 -0500

    wip

---
+++ Lever_Task/GLM_lever_task/CheckTrainingForCrossVal.m
@@ -0,0 +1,25 @@
+%CheckingTrainingForCrossVal 
+ridgeFolds = 10;
+Vm = zeros(size(spikeTrace),'single'); %pre-allocate reconstructed spikeTrace
+randIdx = randperm(size(spikeTrace,1)); %generate randum number index
+foldCnt = floor(size(spikeTrace,1) / ridgeFolds);
+
+
+
+ for iFolds = 1:ridgeFolds
+            dataIdx = true(1,size(spikeTrace,1));
+            dataIdx(randIdx(((iFolds - 1)*foldCnt) + (1:foldCnt))) = false; %index for training data
+            test = ((iFolds - 1)*foldCnt) + (1:foldCnt); 
+            y_here = ones(size(dataIdx));
+            y_here2 = ones(size(test));
+            figure(1)
+            plot(dataIdx, y_here* iFolds); hold on; 
+            figure(2)
+            plot(test, y_here2* iFolds); hold on; 
+ end 
+ 
+%  test = ((9 - 1)*foldCnt) + (1:foldCnt); 
+%  y_here = ones(size(test));
+%  plot(test7, y_here); hold on; 
+%  plot(test8, y_here*2); hold on; 
+%  plot(test9, y_here*3)
