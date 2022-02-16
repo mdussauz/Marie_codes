1b5cd15d3a721d5e8f8b48248843777c4f2d532e
mdussauz <mdussauz@cshl.edu>
2022-02-15 20:53:16 -0500

    cross val check

--- Lever_Task/GLM_lever_task/CheckTrainingForCrossVal.m
+++ Lever_Task/GLM_lever_task/CheckTrainingForCrossVal.m
@@ -10,10 +10,11 @@ foldCnt = floor(size(spikeTrace,1) / ridgeFolds);
             dataIdx = true(1,size(spikeTrace,1));
             dataIdx(randIdx(((iFolds - 1)*foldCnt) + (1:foldCnt))) = false; %index for training data
             test = ((iFolds - 1)*foldCnt) + (1:foldCnt); 
-            y_here = ones(size(dataIdx));
+            test2 = dataIdx(randIdx(((iFolds - 1)*foldCnt) + (1:foldCnt)));
+            y_here = ones(size(test2));
             y_here2 = ones(size(test));
             figure(1)
-            plot(dataIdx, y_here* iFolds); hold on; 
+            plot(test2, y_here* iFolds); hold on; 
             figure(2)
             plot(test, y_here2* iFolds); hold on; 
  end 
@@ -23,3 +24,6 @@ foldCnt = floor(size(spikeTrace,1) / ridgeFolds);
 %  plot(test7, y_here); hold on; 
 %  plot(test8, y_here*2); hold on; 
 %  plot(test9, y_here*3)
+
+figure(3)
+plot(randIdx, ones(size(randIdx)))
