a2aa9234c77107e85bb6071089b7ebf5b2ce5d0c
Marie Dussauze <mdussauz@cshl.edu>
2022-08-29 16:52:50 -0400

    save open fig

---
+++ GeneralUtils/SaveOpenFigure.m
@@ -0,0 +1,34 @@
+%% Save Open Figures
+
+% Enter information
+AnimalName = 'O8';
+SessionDate = '202207062Pr';
+SessionType = 'OL';
+
+% Create filename 
+figureFolder = '/mnt/data/Lever_Task_Figures';
+
+%savepath = '/mnt/data/Lever_Task_Figures/O2_20211011.pdf';
+
+savepath = fullfile(figureFolder,AnimalName,[AnimalName,'_',SessionDate,'_',SessionType,'.pdf']);
+
+% Check if file already exists
+if exist(savepath)
+    reply = input('These figures have already been saved. \nDo you want to overwrite? Y/N [Y]: ','s');
+    if strcmp(reply,'N')
+        return;
+    end
+end
+
+% Get handles to all open figures
+figHandles = findall(0,'Type','figure'); 
+
+% Save first figure
+exportgraphics(figHandles(1),savepath)
+
+% Loop through figures 2:end
+for i = 2:numel(figHandles)
+    exportgraphics(figHandles(i),savepath,'Append',true)
+end
+
+close all; clear all;
