1e7ff39046207a050ccd8faa670b56be3479d804
Marie Dussauze <mdussauz@cshl.edu>
2023-10-11 14:53:34 -0400

    minor

---
+++ Preprocessing_All_Sessions.m
@@ -0,0 +1,17 @@
+%%Preprocessing_All_Sessions
+
+%% Batch O
+% open loop
+%PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O3/O3_20211005_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O7/O7_20220630_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O8/O8_20220702_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O9/O9_20220630_r0.mat',1);
+
+% halts
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O3/O3_20210927_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O3/O3_20210929_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O7/O7_20220702_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O8/O8_20220704_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O9/O9_20220702_r1.mat',1);
+
+%% Batch S
