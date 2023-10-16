d15b3cbee35c885958e2c8281e2ce6e901fca91a
Marie Dussauze <mdussauz@cshl.edu>
2023-10-16 16:28:39 -0400

    preprocessed session list from BatchS

--- Preprocessing_All_Sessions.m
+++ Preprocessing_All_Sessions.m
@@ -15,3 +15,33 @@ PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O8/O8_20220704_r0.mat',1
 PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/O9/O9_20220702_r1.mat',1);
 
 %% Batch S
+%%S1
+%no odor
+%PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S1/S1_20230312_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S3/S3_20230530_r0.mat',1);
+
+%classic open loop 
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S1/S1_20230314_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S3/S3_20230321_r0.mat',1);
+PreprocessSmellocatorData('mnt/grid-hs/pgupta/Behavior/S6/S6_20230727_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S11/S11_20230805_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S12/S12_20230727_r0.mat',1);
+
+%free lever open loop 
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S1/S1_20230403_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S7/S7_20230622_r0.mat',1);
+
+%halt flip 
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S1/S1_20230327_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S3/S3_20230407_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S7/S7_20230608_r0.mat',1);
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S11/S11_20230801_r0.mat',1);
+
+
+%offset 
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S1/S1_20230405_r0.mat',1);
+
+%% Batch S
+%S6
+%halt flip 
+PreprocessSmellocatorData('/mnt/grid-hs/pgupta/Behavior/S6/S6_20230710_r0.mat',1);
