f6a6ddc6cefee615c5402e1e00c3484dcbb05f6f
Marie_Mac <mdussauz@cshl.edu>
2024-01-21 22:27:54 -0500

    minor

--- 1_ Lever_Task_Project/3_OpenLoopReplayAnalysis/Example_Open_Loop_Responses_in_thesis_dissertation.m
+++ 1_ Lever_Task_Project/3_OpenLoopReplayAnalysis/Example_Open_Loop_Responses_in_thesis_dissertation.m
@@ -7,12 +7,11 @@ MySession = '/Users/mariedussauze/Desktop/Analysis/data/Smellocator/Processed/Be
 unitsperfig = 4;
 
 %Modulated units 
-%MyUnits = [50 15 4 33]; % unit 50 = tet9.1 / unit 15 = tet 5.1 / unit 4 = tet 1 / unit 33 = tet 7 > unit 11 to 14 on figure
+%unit 50 = tet9.1 / unit 15 = tet 5.1 / unit 4 = tet 1 / unit 33 = tet 7 > unit 11 to 14 on figure
 
 %Unmodulated units
-MyUnits = [48 14 1 34]; % unit 48 = tet 9 / unit 14 = tet 5.1 / unit 1 = tet 1 > unit 7 to 9 on figure
-% potentially plot 34 or 36 to have a better match for unit 33 on tet 7
-
+% unit 48 = tet 9 / unit 14 = tet 5.1 / unit 1 = tet 1 / unit 34 = tet 7 > unit 7 to 10 on figure
+MyUnits = [48 14 1 34 50 15 4 33]; 
 
 %% get the processed data loaded
 load(MySession, 'Traces', 'PassiveReplayTraces', 'TrialInfo', ...
